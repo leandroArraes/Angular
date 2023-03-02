da0f22b15bbd182c421b01a869c34d97386b7a66
leandroArraes <leandro.arraes.182@gmail.com>
2023-03-02 17:14:51 -0300

    "Atualizando Formularios , inserção de restrições "

--- Estudos/02-Moments/moments/src/app/components/moment-form/moment-form.component.ts
+++ Estudos/02-Moments/moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
-import { Component ,Input} from '@angular/core';
+import { Component ,Input, OnInit} from '@angular/core';
+import { FormControl, FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -7,5 +8,38 @@ import { Component ,Input} from '@angular/core';
 })
 export class MomentFormComponent {
   @Input() btnText!:string
+
+  momentForm!: FormGroup
+
+  ngOnInit():void{
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('',[Validators.required]),
+      description: new FormControl('',[Validators.required]),
+      Image: new FormControl('')
+
+    });
+  }
+
+  get title(){
+    return this.momentForm.get('title')!;
+  }
+
+  get description(){
+    return this.momentForm.get('description')!;
+  }
+
+  get image(){
+    return this.momentForm.get('image')!;
+  }
+
+  submit(){
+    if(this.momentForm.invalid){
+      return;
+    }
+    console.log("enviou o formulário")
+  }
+
+ 
 
 }
